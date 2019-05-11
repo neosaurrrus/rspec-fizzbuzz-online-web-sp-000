0036c8bb7a0a2170d9d74e1baec4920e09f51db4
Lukie Kang <lukiek+f@gmail.com>
2019-05-11 21:41:20 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(x)
+  if x==3
+    "Fizz"
+  elsif x==5
+    "Buzz"
+  elsif x==15
+    "FizzBuzz"
+  end
+end
