ef7731bc1e4353aa96738a82cb7da51485ece948
Lukie Kang <lukiek+f@gmail.com>
2019-05-11 21:35:00 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(x)
-  if x=3 puts "Fizz"
+  if x=3
+    puts "Fizz"
+  elsif x=5
+    puts "Buzz"
+  elsif x=15
+    puts "FizzBuzz"
+  else return nil
 
 end
